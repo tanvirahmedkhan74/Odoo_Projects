efd5716df457e270a12c468cca4f15d30c975052
Tanvir Ahmed Khan <tanvirahmedkhan74@gmail.com>
2023-07-05 14:29:56 +0600

    View Added

---
+++ Tutorial/custom_addons/my_library/__manifest__.py
@@ -0,0 +1,14 @@
+{
+        'name': 'My Library',
+        'summary': 'Manage Books',
+        'description': 'Library Management Application',
+        'author': 'Tanvir Ahmed Khan',
+        'website': 'http://www.youtube.com',
+        'category': 'Uncategorized',
+        'version': '13.0.1',
+        'depends': ['base'],
+        'data': [
+                    'views/library_book.xml',
+                ]
+
+}
